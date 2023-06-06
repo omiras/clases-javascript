6892060d559976b2b53967e0cb1c89ddd88dc6ed
Oscar Miras <oscar.miras@gmail.com>
2023-06-06 10:01:09 +0200

    updated

--- ex1.js
+++ ex1.js
@@ -1,18 +1,18 @@
-// ex1: Completa el método toString de la siguiente clase. Este método debe devolver un 'string' con el siguiente formato : 'The price of the $product is $price €". Por ejemplo, para el objeto 'café', debería mostrar por consola 'The proce of the café Bona is 5 €'
+// ex1: Completa el método toString de la siguiente clase. Este método debe devolver un 'string' con el siguiente formato : 'The price of the $product is $price €". Por ejemplo, para el objeto 'café', debería mostrar por consola 'The price of the café Bona is 5 €'
 
 class Product {
-    constructor(name, price) {
-        this.name = name
-        this.price = price
-    }
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+  }
 
-    toString() {
-        return this.price // INCOMPLETO
-    }
+  toString() {
+    // 'The price of the café Bona is 5 €'
+    return this.price; // INCOMPLETO
+  }
 }
 
-const cafe = new Product('café Bonka', 5)
-console.log(cafe.toString())
-const bollo = new Product('bolleria industrial', 3)
-console.log(bollo.toString())
-
+const cafe = new Product("café Bonka", 5);
+console.log(cafe.toString());
+const bollo = new Product("bolleria industrial", 3);
+console.log(bollo.toString());
--- ex2.js
+++ ex2.js
@@ -1,24 +1,18 @@
 class Coche {
+  /**
+   *
+   * @param {string} modelo Representa el modelo de coche (marca, serie, etc)
+   * @param {string} matricula Mátricula
+   * @param {boolean} gps Indica si lleva navegación GPS integrada
+   * @param {number} deposito Indica cuan lleno está el depósito. 100 indica que está completamente lleno, 0 que esta vacío.
+   */
+  constructor(modelo, matricula, gps) {}
 
-    /**
-     * 
-     * @param {string} modelo Representa el modelo de coche (marca, serie, etc)
-     * @param {string} matricula Mátricula 
-     * @param {boolean} gps Indica si lleva navegación GPS integrada
-     * @param {number} deposito Indica cuan lleno está el depósito. 100 indica que está completamente lleno, 0 que esta vacío.
-     */
-    constructor(modelo, matricula, gps) {
+  // Llena el deposito a 100 unidades de gasolina
+  llenarDeposito() {}
 
-    }
-
-    // Llena el deposito a 100 unidades de gasolina
-    llenarDeposito() {
-    }
-
-    // Consume una unidad del deposito
-    usar() {
-    }
-
+  // Consume una unidad del deposito
+  usar() {}
 }
 
 const astra = new Coche("Opel Astra", "1984 GI", false);
@@ -26,21 +20,17 @@ const ibiza = new Coche("Seat Ibiza", "2021 AZ", false);
 
 // PRUEBAS
 
-//1. El console.log debería mostrar una instancia del tipo 'Coche', con el modelo, matrícula e indicando si posee o no gps.
-console.log("Coche Astra:", astra)
+//1. El console.log debería mostrar una instancia del tipo 'Coche', con el modelo, matrícula e indicando si posee o no gps. El deposito tendría que estar lleno
+console.log("Coche Astra:", astra);
 
 //2. Despúes de invocar el método 'usar', el depósito del coche debería tener 99 unidades
-astra.usar()
-console.log("Coche Astra después de usarlo:", astra)
+astra.usar();
+console.log("Coche Astra después de usarlo:", astra);
 
 //3. Después de invocar el método 'llenarDeposito', el coche debería tener 100 unidades de gasolina en el deposito
-astra.llenarDeposito()
-console.log("Coche Astra después de usarlo:", astra)
-
+astra.llenarDeposito();
+console.log("Coche Astra después de llenar el depósito:", astra);
 
 //4. Implementa un NUEVO MÉTODO en la clase Coche, que devuelve un bool. Este método se va a llamar 'estaDepositoVacio'. Debe devolver 'true' si al deposito le quedan 0 unidades de gasolina
-ibiza.deposito = 0
-console.log("El deposito del Ibiza debería estar vacío", ibiza)
-
-
-
+ibiza.deposito = 0;
+console.log("El deposito del Ibiza debería estar vacío", ibiza);
