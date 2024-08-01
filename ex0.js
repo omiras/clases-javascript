f67dec52be8c4101cbde6718668247ea1eaab40f
omiras <oscar.miras@gmail.com>
2024-08-01 20:45:28 +0200

    ex0 sol

--- ex0.js
+++ ex0.js
@@ -1,7 +1,7 @@
 /**
  * Ejercicio: Usa el contructor new Date adecuadamente para crear un objeto de tipo Date que se inicialice con tu fecha de nacimiento https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/Date
  */
-const fechaNacimiento = new Date(); // <--- SOLAMENTE DEBES MODIFICAR ESTA LÍNEA
+const fechaNacimiento = new Date('1982-05-19'); // <--- SOLAMENTE DEBES MODIFICAR ESTA LÍNEA
 
 // NO MODIFICAR A PARTIR DE AQUÍ
 const fechaActual = new Date();
